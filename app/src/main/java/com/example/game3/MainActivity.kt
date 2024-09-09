39c536d013d80a3d19985835f1ff91bd7788a391
Vidula Gunasekara <vidulashavinda12@gmail.com>
2024-09-09 15:11:01 +0530

    File Upload

---
+++ app/src/main/java/com/example/game3/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.game3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.game3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playOfflineBtn.setOnClickListener{
+            createOfflineGame()
+        }
+
+    }
+
+    fun createOfflineGame(){
+        GameData.saveGameModel(
+            GameModel(
+                gameStatus = GameStatus.JOINED
+            )
+        )
+        startGame()
+    }
+
+    fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java))
+    }
+}
